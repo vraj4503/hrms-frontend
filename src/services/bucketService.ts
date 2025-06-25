62f81dbbbec486a8da2d4b5704457e5ee76a80fa
vraj4503 <129033176+vraj4503@users.noreply.github.com>
2025-06-25 10:23:30 +0530

    Update bucketService.ts

--- src/services/bucketService.ts
+++ src/services/bucketService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/Bucket'; 
+const API_URL = 'hrms-backend-production-3091.up.railway.app/Bucket'; 
 
 export interface Bucket {
   BucketId?: number;
