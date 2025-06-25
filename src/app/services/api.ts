a8d44a9ed2d0babf7b9ce24ff4964d34724e2c3a
vraj4503 <129033176+vraj4503@users.noreply.github.com>
2025-06-25 10:25:13 +0530

    Update api.ts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'hrms-backend-production-3091.up.railway.app';
 
 export interface User {
   UID: number;
